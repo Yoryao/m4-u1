4e2d607cd2e863d274d79da8dc2fea0ea7d671ad
yoryao <jorgejoaquinrivera@gmail.com>
2021-06-04 16:32:00 -0300

    Apis listas, vinculación del post html - mysql funciona, falta vincular otras apis.

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 const express = require('express');
 const mysql = require('mysql');
 const util = require('util');
+const router = express.Router();
 
 //declaro la variable app para utilizar el express.
 var app = express();
@@ -10,7 +11,8 @@ var app = express();
 const port = 3000;
 
 //uso de los datos atraves de URL
-app.use(express.urlencoded());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("peliculas"));
 
 //mapeo de petición a JSON 
 app.use(express.json());
@@ -20,110 +22,123 @@ app.use(express.json());
 //busqueda de todas las peliculas: GET + localhost:3000/peliculas
 app.get('/peliculas', async(req, res) => {
     try {
-        let query = 'SELECT * FROM peliculas'; //declaro la consulta
 
+        const query = 'SELECT * FROM peliculas'; //declaro la consulta
         const answer = await qy(query); //capturo la consulta
 
-        //resultado en la consola.
-        console.log('Impresión de la consulta por consola: ', answer);
+        //valido que haya peliculas en base de datos.
+        if (answer.length <= 0) {
+            const volver = '<!DOCTYPE html><html lang="en"><head><meta charset="UTF-8"><meta http-equiv="X-UA-Compatible" content="IE=edge"><meta name="viewport" content="width=device-width, initial-scale=1.0">            <title>Formulario de registro</title></head><body>        <header><h1>Random Movies</h1><h3>No hay peliculas por mostrar</h3></header><button><a href="index.html">Volver al registro</a></button></body><script src="app.js"></script></html>';
+            res.status(413).send(volver);
+            return;
+        }
 
         //resultado en el browser
         res.send({ 'Impresión por HTML: ': answer });
-
-    } catch (e) { //atrapo el error en el metodo
-        console.log(e.message); //imprimo por consola
-        res.status(413).send({ 'Error por Html: ': e.message }); //imprimo por html.
+    } catch (e) {
+        console.log(e.message);
+        res.status(413).send('Error inesperado');
     }
 });
+
 
 //ingreso de peliculas: POST + localhost:3000/peliculas + json {id, titulo, genero}
 app.post('/peliculas', async(req, res) => {
     try {
-        let query = 'INSERT INTO peliculas (id, titulo, genero) VALUE (?, ?, ?)';
 
-        const answer = await qy(query, [req.body.id, req.body.titulo, req.body.genero]);
+        const volver = '<!DOCTYPE html><html lang="en"><head><meta charset="UTF-8"><meta http-equiv="X-UA-Compatible" content="IE=edge"><meta name="viewport" content="width=device-width, initial-scale=1.0">            <title>Formulario de registro</title></head><body>        <header><h1>Random Movies</h1><h3>Faltan cargar datos</h3></header><button><a href="index.html">Volver al registro</a></button></body><script src="app.js"></script></html>';
 
+        //valido recibir los datos para crear un registro.
+        if (req.body.titulo == '' || req.body.genero == '') {
+            res.status(413).send(volver);
+            return;
+        }
 
-        console.log('Impresión por consola de la carga: ' + answer); //esto imprime bien. 
+        const titulo = req.body.titulo;
+        const genero = req.body.genero;
 
-        res.send('Impresión por pagina de la carga:' + answer); // usarlo mas tarde para imprimir confirmación de carga. 
+        //Ingreso la pelicula
+        const query = 'INSERT INTO peliculas ( titulo, genero) VALUE ( ?, ?)';
+
+        const answer = await qy(query, [titulo, genero]);
+
+        const exito = '<!DOCTYPE html><html lang="en"><head><meta charset="UTF-8"><meta http-equiv="X-UA-Compatible" content="IE=edge"><meta name="viewport" content="width=device-width, initial-scale=1.0">            <title>Formulario de registro</title></head><body>        <header><h1>Random Movies</h1><h3>La sugerencia se realizo con exito. Pelicula: ' + titulo + '</h3></header><button><a href="index.html">Volver al registro</a></button></body><script src="app.js"></script></html>';
+        // usarlo mas tarde para imprimir confirmación de carga.
+
+        res.send(exito);
 
     } catch (e) {
-        console.log(e.message); //imprimo por consola
-        res.status(413).send({ 'Error por Html: ': e.message }); //imprimo por html.
+        console.log(e.message);
+        res.status(413).send('Error inesperado');
     }
 });
 
+//eliminado de un registro mediante el titulo. DELETE + localhost:3000/pelicula/titulo 
 
-//NO FUNCIONA!!
-//eliminado de un registro mediante el titulo. DELETE + localhosto:3000/pelicula/titulo 
-app.delete('/peliculas/:titulo', async(req, res) => {
+app.delete('/peliculas/:id', async(req, res) => {
     try {
-        let query = 'DELETE FROM peliculas WHERE titulo = ?';
 
-        const answer = await qy(query, req.body.titulo);
+        //valido recibir los datos para crear un registro.
+        if (req.body.id == '') {
+            res.status(413).send(volver);
+            return;
+        }
+
+        //valido si existe la pelicula
+        const queryExiste = 'SELECT * FROM peliculas WHERE id = ?';
+        const respuestExiste = await qy(queryExiste, [req.params.id]);
+
+        if (respuestExiste.length <= 0) {
+            res.status(413).send({
+                mensaje: 'No existe ese libro'
+            });
+            return;
+        }
+
+        const query = 'DELETE  FROM peliculas WHERE id = ?';
+        const answer = await qy(query, req.params.id);
 
         console.log(answer);
         res.status(413).send('Se ha borrado la pelicula.');
 
     } catch (e) {
-        console.log(e.message); //imprimo por consola
-        res.status(413).send({ 'Error por Html: ': e.message }); //imprimo por html.
+        console.log(e.message);
+        res.status(413).send('Error inesperado');
     }
 
 });
 
-//Funciona!
-//Modificar el titulo segun el ID : put + localhost:3000/peliculas/id => json {"titulo" : "tituloNuevo"}
+//Modificar el genero segun el ID : put + localhost:3000/peliculas/id => json {"genero" : "generoNew"}
 app.put('/peliculas/:id', async(req, res) => {
 
     try {
 
-        const { id } = req.params;
-        const tituloNuevo = req.body;
+        //valido si existe la pelicula
+        const queryExiste = 'SELECT * FROM peliculas WHERE id = ?';
+        const respuestExiste = await qy(queryExiste, [req.params.id]);
 
+        if (respuestExiste.length <= 0) {
+            res.status(413).send({
+                mensaje: 'No existe esa pelicula'
+            });
+            return;
+        }
 
-        let query = 'UPDATE peliculas SET ? WHERE id = ?';
+        const id = req.params.id;
+        const generoNew = req.body.genero;
 
-        const answer = await qy(query, [tituloNuevo, id]);
+        let query = 'UPDATE peliculas SET genero = ? WHERE id = ?';
+
+        const answer = await qy(query, [generoNew, id]);
 
         console.log(answer);
         res.status(413).send('Se ha actualizado el ID: ' + id);
 
     } catch (e) {
-        console.log(e.message); //imprimo por consola
-        res.status(413).send({ 'Error por Html: ': e.message }); //imprimo por html.
+        console.log(e.message);
+        res.status(413).send('Error inesperado');
     }
 });
-
-//No funciona. no cambia el genero en Base de datos, pero no captura ningun error.
-//Modificar el genero segun el Titulo : put + localhost:3000/peliculas/titulo => json {"genero" : "generoNuevo"}
-app.put('/peliculas/:titulo', async(req, res) => {
-
-    try {
-
-        const { titulo } = req.params;
-        const generoNuevo = req.body;
-
-
-        let query = 'UPDATE peliculas SET ? WHERE titulo = ?';
-
-        const answer = await qy(query, [generoNuevo, titulo]);
-
-        console.log(answer);
-        res.status(413).send('Se ha actualizado el genero de la pelicula: ' + titulo);
-
-    } catch (e) {
-        console.log(e.message); //imprimo por consola
-        res.status(413).send({ 'Error por Html: ': e.message }); //imprimo por html.
-    }
-});
-
-
-////////////////////////
-// res.status(200).json(respuesta); 
-///////////////////////
-
 
 
 
@@ -133,7 +148,7 @@ const conn = mysql.createConnection({
     port: 3306,
     user: 'root',
     password: '',
-    database: 'recomendados',
+    database: 'randommovies',
 });
 //manejo el posible error en la conexcion.
 conn.connect((error) => {
@@ -150,6 +165,9 @@ const qy = util.promisify(conn.query).bind(conn);
 
 
 
-app.listen(port, () => {
-    console.log('Servidor operando en el puerto: ', port);
+// mostrar el puerto de conexion y escucha de la app.
+app.listen(process.env.PORT || port, () => {
+    console.log("Servidor iniciado en puerto: " + port);
 });
+
+module.exports = router;
